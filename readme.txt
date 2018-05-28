0d016fe8e0ec802292cf502b37bfe95ea9dd6244
haojie <782319624@qq.com>
2018-05-28 16:29:12 +0800

    no message

--- readme.txt
+++
@@ -1,6 +0,0 @@
-Git is a distributed version control system.
-Git is free software.
-AAAAAA
-BBBBBB
-cccccc
-dddddd
--- zhongjiu/js/zhuce.js
+++
@@ -1,3 +0,0 @@
-var op = document.getElementsByTagName("p");
-var ma =op[1].getElementsByTagName("mark")[0];
-console.log(op);
