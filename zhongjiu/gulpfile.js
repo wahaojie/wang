273334a49e6ca06b9868fce51c611f4f7838c72b
haojie <782319624@qq.com>
2018-05-31 20:21:31 +0800

    no message

--- zhongjiu/gulpfile.js
+++ zhongjiu/gulpfile.js
@@ -1,6 +1,3 @@
-var gulp =require('gulp'); 
-gulp.task('copy-index',function(){ 
-	//gulp.src()找到我们的index.html然后使用.pipe()通道 
-	//gulp.dest()发布拷贝 
-	return gulp.src('index.html').pipe(gulp.dest('dist'));
- });
+ var gulp = require('gulp');
+ gulp.task('hello',function(){ //第一个参数是任务名称，第二个参数是任务功能
+}) 
